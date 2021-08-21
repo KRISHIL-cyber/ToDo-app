85be3b533d5870905df03ba4b4d9a561859e048f
KRISHIL-cyber <krishil005@gmail.com>
2021-08-21 23:01:45 +0530

    Final Commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,20 +1,42 @@
-const express = require('express');
+const mongoose = require('mongoose');
 
-var data= [{item: ' buy veggies'}, {item: 'Do assignments'}, {item: 'watch animes'}];
+const DB = "mongodb+srv://tester:test007@test.odevx.mongodb.net/todo?retryWrites=true&w=majority";
+mongoose.connect(DB, {useNewUrlParser: true });
+
+var todoSchema= new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo',todoSchema);
+var itemOne= Todo({item: 'watch animes'}).save(err=>{
+    if(err) throw err;
+    console.log('Item saved');
+});
+
 
 module.exports= (app)=>{
 
     
     app.get('/todo', (req,res)=>{
-        res.render('todo', {todos: data});
+        //get data from mongodb and pass it to view
+        Todo.find({}, (err,data)=>{
+            if (err) throw err;
+            res.render('todo', {todos: data});
+        });   
     });
 
     app.post('/todo', (req,res)=>{
-        data.push(req.body);
-        res.json({todos: data});  //as we are reloading the same page so to match with todo.ejs code
+        var newTodo = Todo(req.body).save((err,data)=>{
+            if (err) throw err;
+            res.json({todos:data}); //as we are reloading the same page so to match with todo.ejs code
+        });
     });
 
-    app.delete('/todo', (req,res)=>{
-
+    app.delete('/todo/:item', (req,res)=>{
+        //delete the requested item from mongodb
+        Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove((err,data)=>{
+            if (err) throw err;
+            res.json({todos: data});
+        });
     });
 };
